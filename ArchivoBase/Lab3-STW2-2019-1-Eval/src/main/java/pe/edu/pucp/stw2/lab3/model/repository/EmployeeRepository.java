8e7e8e8683f2b820f94b466469e3451d08cb1c7a
atl1998 <167376701+atl1998@users.noreply.github.com>
2024-09-07 16:50:17 -0500

    archivo base eliminado
    la tarea se encuentra en la carpeta tarea 2

--- ArchivoBase/Lab3-STW2-2019-1-Eval/src/main/java/pe/edu/pucp/stw2/lab3/model/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.pucp.stw2.lab3.model.repository;
-import org.springframework.data.jpa.repository.Query;
-import pe.edu.pucp.stw2.lab3.model.bean.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-
-}
